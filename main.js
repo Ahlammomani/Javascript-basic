292a17f4ba71b3896b5ac55daf2dfa296b8d29f4
ahlammomani <ahlam.hani2002@gmail.com>
2024-12-26 16:21:08 +0300

    java task

---
+++ main.js
@@ -0,0 +1,36 @@
+console.log(-5*3)
+console.log("javascript" + 50)
+console.log(17 % 5)
+console.log(5 % 17)
+console.log(5/10)
+console.log(4 === "4")
+console.log(4 != 5)
+console.log(7<=8)
+console.log("hello"+5)
+let x = 7;
+console.log(Math.ceil(x)- Math.floor(x))
+console.log(Math.pow(x,2))
+// Q2
+let usernumber = prompt("Please enter a number:");
+let number = Number (usernumber)
+alert("You entered: " + number);
+// Q3
+let num1 = prompt(" Enter the first number");
+let num2 = prompt(" Enter the second number");
+let sum = Number(num1) + Number(num2);
+alert("The sum of the two numbers is: " + sum);
+// Q4
+let num3 = prompt(" Enter the first number:");
+let num4 = prompt("Enter the second number:");
+let product = Number(num3) * Number(num4);
+alert("The product of the two numbers is: " + product);
+// Q5
+let num5 = prompt("Q5: Enter the first number:");
+let num6 = prompt("Q5: Enter the second number:");
+let division = Number(num5) / Number(num6);
+alert("The result of division is: " + division);
+// Q6
+let a=44
+let b=6
+let totalsum = a+b;
+alert("The sum of " + a + " and " + b + " is: " + totalsum)
